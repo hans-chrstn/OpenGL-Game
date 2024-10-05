bda5ea2b9e063607e8521651c9b8b556f0b641b8
hayato-oo <xuhiko13@gmail.com>
2024-10-05 02:25:59 -0400

    added esc to close logic; added colorscheme

--- src/main.cpp
+++ src/main.cpp
@@ -43,12 +43,22 @@ private:
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   }
 
+  void processInput(GLFWwindow* window) {
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, true);
+    }
+  }
+
   static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
   }
 
   void mainLoop() {
     while (!glfwWindowShouldClose(window)) {
+      processInput(window);
+
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
       glfwSwapBuffers(window);
       glfwPollEvents();
     }
